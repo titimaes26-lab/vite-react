f5ce21e1b9ae5004885bde7897ecde0c245d62f8
titimaes26-lab <titi.maes26@gmail.com>
2026-03-13 22:52:32 +0100

    Create Main.jsx

---
+++ Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RestaurantManager from './restaurant-manager.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RestaurantManager />
+  </React.StrictMode>,
+)
